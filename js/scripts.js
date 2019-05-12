83c024c1389173b6e2a9d3ae414306e2c98e3c4f
Alan <mashalonzo741@gmail.com>
2019-05-13 00:03:47 +0300

    create the switch player function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 /* Back End */
+
+function Player(playerName, playerNumber, score) {
+  this.playerName = playerName;
+  this.playerNumber = playerNumber;
+  this.score = score;
+}
+Player.prototype.scorePoints = function(points) {
+  this.score += points;
+};
+Player.prototype.resetScore = function() {
+  this.score = 0;
+};
+function switchPlayer(currentPlayerNumber, numberOfPlayers) {
+  numberOfPlayers -= 1;
+  var nextPlayer = 0;
+  if (currentPlayerNumber === numberOfPlayers) {
+    nextPlayer = 0;
+  } else {
+    nextPlayer = currentPlayerNumber + 1;
+  }
+  return nextPlayer;
+}
